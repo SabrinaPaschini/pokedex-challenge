8a6c73455285e78d411b0d119b150ba0ec873b97
Sabrina Paschini <sabrina.paschini@hotmail.com>
2023-10-22 16:31:38 -0300

    css

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,32 +4,40 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
-
-
 function convertPokemonTypesToli(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class"type-api">${typeSlot.type.name}</li>`)
+
+    return pokemonTypes.map((typeSlot) => `<li class"type-api" >${typeSlot.type.name}</li>`)
 }
 
 function convertPokemonToLi(pokemon) {
     return `
-    <li class="pokemon">
+    <li class="pokemon ${pokemon.name}  >
 
         <span class="number"> ID: #${pokemon.id}</span>
         <span class="name">${pokemon.name}</span>
-    <div class="detail">
+    <div class="detail" >
     <img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg">
 
-        <ol class="type-api">
+        <ol class="type-api" >
         
              ${convertPokemonTypesToli(pokemon.types).join('')}
          
                 </ol>
            </div>
-           
 </li>
 
+<section class="content">
+
+<ol class="pokemons" id="pokemonList"></ol>
+</section>
+
+
     `
+ 
+    
 }
+ 
+
 
 const pokemonList = document.getElementById('pokemonList')
 
